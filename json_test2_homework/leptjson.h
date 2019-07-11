b11be8c5588f6be83b4cbec234f06ab703e662e3
AaronZhang <xihaungzhang95@163.com>
2019-07-11 21:24:51 +0800

    test_2

--- json_test2_homework/leptjson.h
+++ json_test2_homework/leptjson.h
@@ -3,7 +3,8 @@
 
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;   //枚举
 
-typedef struct {
+typedef struct {  //表示一个节点
+	double n;//选择double来储存json数字，仅当type==LEPT_NUMBER时，n才表示JSON数字的数值
     lept_type type;
 }lept_value;
 
@@ -11,11 +12,14 @@ enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_ROOT_NOT_SINGULAR,
+	LEPT_PARSE_NUMBER_TOO_BIG
 };
 
 int lept_parse(lept_value* v, const char* json);  //解析函数声明，其中v为根节点，json为json文本（c风格的字符串）
 
 lept_type lept_get_type(const lept_value* v);
+
+double lept_get_number(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
